3c47ed75e0d820ab513db41feba52badcb68e06b
Oleg Kovalov <oleg@hey.com>
2024-03-02 12:15:50 +0100

    Add X (#7)

--- x.go
+++ x.go
@@ -1,5 +1,15 @@
 package dbg
 
-// Sink any value like it's used.
-// Treat it as `_ = x` or `_, _, ... = x, y, ...`.
-func Sink(values ...any) {}
+import (
+	"fmt"
+)
+
+// X will print value and return it. Read `X` as `X-ray`.
+// Can be used in any expression:
+//
+//	req, err := http.Post(dbg.X(url), "localhost:8080/ping", http.NoBody)
+func X[T any](value T) T {
+	loc := Location(1)
+	fmt.Fprintf(output, "[DEBUG] %s: %v", loc, value)
+	return value
+}
